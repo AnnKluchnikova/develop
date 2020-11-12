69b235c06526acdb23b69b54952c0e1f49f00a3f
Анна <anna.klyuchnikova@eltex.loc>
2020-11-12 10:08:39 +0700

    Заг.ф для функций работы с файлами
    Имеющиеся функции предназначены для открытия, считывания и сохранения
    в указанный файл.

---
+++ f_database_file.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/*Функция проверки доступности файла и проверки его содержимого*/
+int read_file(char const *file_path);
+
+/*Функция сохранения изменений в файл*/
+int save_to_file(char const *file_path);
+
