6bea695fd23fe353923ea021407d2c962b6bbd83
Анна <anna.klyuchnikova@eltex.loc>
2020-11-17 14:12:56 +0700

    V1.1 Функции работы со списками
    Добавлено:
    
    • Из имеющегося ранее отдельного файла filework.c были перенесены
    функции read_file, save_to_file, которые читают списки из файла и
    записывают их в файл (сохраняют). Это сделано из-за того, что
    выделение таких функций от всех остальных функций работы со списками
    не имеет смылсла, т.к. все функции не унифицированы и какие-то зависят
    друг от друга.
    
    Изменения:
    
    • В функции read_file были добавлены сообщения о количестве прочитанных
    строк из файла при отсутствии ошибок, сообщения о номере строки, в котором
    была найдена ошибка.
    
    • В связи с предыдущими изменениями нумерация строк при выводе их на экран
    будет начинаться с "1", а не с "0", т.к. стандартные редакторы начинают
    нумерацию с "1", из-за чего пользователь может запутаться при поиске строки
    с ошибкой, если нумерация в данной программе будет с "0".

--- listedit.h
+++ listedit.h
@@ -3,7 +3,7 @@
 #include "types.h"
 
 /*Проверка базы на непустоту*/
-int not_empty_database();
+int not_empty_database(void);
 
 /*Функция передачи указателя на уже существующий адрес*/
 address_t *get_existing_address(char *street_name, int home_num);
@@ -15,25 +15,25 @@ address_t *add_new_address(char *street_name, int home_num);
 void add_new_person(char *name, unsigned int age, char *street_name, int home_num, int special_label);
 
 /*Функция проверки полученных данных для последующего добавления*/
-int add_correct_data_to_database(char *name, char *age, char *street_name, char *home_num, int call);
+int add_correct_data_to_database(char *name, char *age, char *street_name, char *home_num, int show_message);
 
 /*Функция считывания данных пользователя с консоли*/
-int get_data_to_add();
+int get_data_to_add(void);
 
 /*Вывод списка имен на экран*/
-void view_all_lists();
+void view_all_lists(void);
 
 /*Вывод списка адресов на экран*/
-void view_list_of_address();
+void view_list_of_address(void);
 
 /*Очистка списка адресов*/
-int clear_list_of_address();
+int clear_list_of_address(void);
 
 /*Очистка списка людей*/
-int clear_list_of_people();
+int clear_list_of_people(void);
 
 /*Полная очистка списка*/
-int clear_all_lists(int call);
+int clear_all_lists(int show_message);
 
 /*Поиск элемента по частичному совпадению с именем*/
 int search_record_by_name_pattern(char *name);
@@ -42,7 +42,7 @@ int search_record_by_name_pattern(char *name);
 void search_record_by_name(char *name);
 
 /*Получения корректного имени для дальнейшего поиска в базе*/
-int get_name_to_serch();
+int get_name_to_serch(void);
 
 /*Вывод сообщений после работы функции удаления записи*/
 int message_about_delete(int items_deleted);
@@ -57,7 +57,7 @@ int delete_address_record(address_t *address);
 int delete_person_record(char *name);
 
 /*Получение корректного имени для последующего удаления записи*/
-int get_name_to_delete();
+int get_name_to_delete(void);
 
 /*Функция проверки доступности файла и проверки его содержимого*/
 int read_file(char const *file_path);
