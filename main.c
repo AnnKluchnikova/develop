c831ada3e59eecf4460088e6f282d34b7f8d4faf
Анна <anna.klyuchnikova@eltex.loc>
2020-11-25 15:42:39 +0700

    v1.1.2 Основная программа
    Добавлено:
    • Функция-обработчик сигналов SIGINT, SIGUSR1, SIGUSR2. При этом: сигнал прерывания действует аналогично команде "exit",
    которая завершает программу с предворительной проверкой наличия изменений (если есть, то спрашивает о сохранении в файл).
    Остальные сигналы обрабатываются путем вывода на экран сообщения о получении сигналов.
    
    Изменено:
    • Чтобы обработать прерывание, было необходимо добавить флаг выхода (exit_flag) и перенести переменную file_path в другую
    область видимости - сделать глобальной переменной.

--- main.c
+++ main.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h> // для free(), calloc()
-#include <signal.h> // для signal()
+#include <signal.h> // для signal(), для raise()
 
 #include "commands.h"
-#include "setdata.h"
 #include "listedit.h"
 #include "types.h"
 
 #define DEFAULT_FILE "./database.txt"
 
+unsigned char exit_flag = 0; // Флаг выхода из программы по сигналу Ctrl+C
+char const *file_path;      // Путь до файла, с данными которого будет работать программа
+
+/*Функция обработки сигналов*/
+void signal_handler(int signo)
+{
+    if(signo == SIGINT)
+    {
+        execute_command(CMD_EXIT, file_path);
+        exit_flag = 1;
+    }
+    else if(signo == SIGUSR1)
+        printf("Received SIGUSR1!\n");
+    else if(signo == SIGUSR2)
+        printf("Received SIGUSR2!\n");
+}
+
 /*Головная фунция, которой может передаваться путь до файла при запуске программы*/
 int main(int argc, char const *argv[])
 {
-    char const *file_path;
-
     if(argc == 2)
         file_path = argv[1];
     else
@@ -27,16 +41,18 @@ int main(int argc, char const *argv[])
 
     printf("\n*** Welcome to the people database editor! ***\n"
            " You are editing a file: %s\n"
-           "\n Enter the 'show commands' command to view the command reference.", file_path);
+           "\n Enter the 'show commands' command to view the command reference.\n", file_path);
 
     change_flag = 0;
     int cmd_num = 0;
 
-    signal(SIGINT, SIG_IGN);
+    signal(SIGINT, signal_handler);
+    signal(SIGUSR1, signal_handler);
+    signal(SIGUSR2, signal_handler);
 
-    while(cmd_num != CMD_EXIT)
+    while((exit_flag == 0) && (cmd_num != CMD_EXIT))
     {
-        printf("\n >");
+        printf(">");
         cmd_num = process_command_name();
         execute_command(cmd_num, file_path);
     }
