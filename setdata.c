14bfae64650374aaa5728e22da102cde7eccb44b
Анна <anna.klyuchnikova@eltex.loc>
2020-12-08 12:47:58 +0700

    v2.1 Функции обработки данных
    Изменено: Исключена "единая точка выхода" с goto из-за того, что в функциях с одной проверкой
    и одним вариантом выхода по ошибке нет смысла такое использовать, так как в блоке проверки
    можно сразу написать действия при выходе, а не перемещаться сначала на метку goto.

--- setdata.c
+++ setdata.c
@@ -11,31 +11,33 @@
 /*Функция для считывания данных*/
 int read_data(char *buf, int data_len)
 {
+    char *trigger = NULL;
+
     fgets(buf, data_len, stdin);
     __fpurge(stdin);
 
-    char *trigger = strchr(buf, '\n'); // Избавление от символа конца строки необходимо
-                                      // во избежания проблем со сравнением данных,
+    trigger = strchr(buf, '\n');       // Избавление от символа конца строки необходимо
+                                      // во избежании проблем со сравнением данных,
     if (trigger != NULL)             // т.к. сравниваемые данные в основном не имеют такого символа
         *trigger = '\0';            // и могут отличаться только "\n" в конце строки
     else
-        goto false_data;          // Но если в строке не найдется символа конца строки,
-                                 // значит строка была введена длиннее, чем ожидалось
+    {
+        printf("\nERROR! "
+               "The string is longer than the expected size.\n");  // Но если в строке не найдется символа конца строки,
+        return ERROR;                                             // значит строка была введена длиннее, чем ожидалось
+    }
 
     return VALID;
-
-false_data:
-    printf("\nERROR! The string is longer than the expected size.\n");
-    return ERROR;
 }
 
 /*Функция для проверки непустоты строки*/
-int is_not_null_string(char *buf)
+int is_not_null_string(const char *buf)
 {
+    int simbols = 0;
+
     if(buf == NULL)        // Проверка на наличие пустой строки в непустом файле
         goto false_data;  // Без этого условия программа падает при обработке такой строки
 
-    int simbols = 0;
     for(int index = 0; index < strlen(buf); index++) // Проверяется, что строка содержит значимые символы,
         if(buf[index] != ' ')                       // а не заполнена пробелами
             simbols++;                             // В базе не может быть пустых значений или только пробелы
@@ -51,33 +53,34 @@ false_data:
 }
 
 /*Функция проверки строковых данных на корректность*/
-int is_correct_string(char *buf)
+int is_correct_string(const char *buf)
 {
     if(is_not_null_string(buf) != VALID)
         return ERROR;
 
     /*if(strchr(buf, ',') != NULL)    // Запятая является разделителем между разными данными в файле
-        goto false_data;           // Если она окажется в принимаемой строке, то при сохранении в файл и
-                                  // считывании из этого файла данные будут восприниматься не корректно
-*/ // ATTANTION Если используется файл формат json, то это действие не нужно
+        return ERROR;                // Если она окажется в принимаемой строке, то при сохранении в файл и
+                                    // считывании из этого файла данные будут восприниматься не корректно
+*/ // ATTANTION Если используется файл формата json, то это действие не нужно
 
     if(strlen(buf) >= STRING_LEN)
-        goto false_data;
+    {
+        printf("\nERROR! The string is longer than 63 characters.\n");
+        return ERROR;
+    }
 
     return VALID;
-
-false_data:
-    printf("\nERROR! The string is longer than 63 characters or contains ','.\n");
-    return ERROR;
 }
 
 /*Функция для преобразования возраста из строки в число*/
-int get_age_as_number(char *buf)
+int get_age_as_number(const char *buf)
 {
+    int get_number = -1;
+
     if(is_not_null_string(buf) != VALID)
         return ERROR;
 
-    int get_number = atoi(buf);
+    get_number = atoi(buf);
 
     if((get_number > 0)&&(get_number < MAX_AGE))
         return get_number;
@@ -90,20 +93,23 @@ int get_age_as_number(char *buf)
 }
 
 /*Функция для преобразования номера дома из строки в число*/
-int get_home_number_as_number(char *buf)
+int get_home_number_as_number(const char *buf)
 {
+    int get_number = -1;
+
     if(is_not_null_string(buf) != VALID)
         return ERROR;
 
     if(strlen(buf) >= NUMBER_LEN)
-        goto false_data;
+    {
+        printf("\nERROR! House number doesn't match the real one.\n");
+        return ERROR;
+    }
 
-    int get_number = atoi(buf);
+    get_number = atoi(buf);
 
     if(get_number > 0)
         return get_number;
 
-false_data:
-    printf("\nERROR! House number doesn't match the real one.\n");
-    return ERROR;
+    return VALID;
 }
