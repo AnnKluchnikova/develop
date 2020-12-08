3f7598b35dcf63b0c68bc2eec9fdeed7615f5413
Анна <anna.klyuchnikova@eltex.loc>
2020-12-08 13:11:21 +0700

    v2.1.2 Заг.файл с описанием функций работы с форматом json
    Изменено: Заменено описание функции считывания данных из файла

--- getjson.h
+++ getjson.h
@@ -1,7 +1,7 @@
 #ifndef GETJSON_H
 #define GETJSON_H
-/*Функция преобразует внутренний формат данных в формат json*/
-int add_json_object_to_database(char const *file_path);
+/*Функция считывает данные файла json и записывает их во внутреннюю структуру*/
+int read_json_file(char const *file_path);
 
 /*Функция записывает форматированный данные в файл json*/
 int write_data_to_json_file(char const *file_path);
